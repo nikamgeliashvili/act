905eb5eb6a2cae8db3c64a1dba8d4a94e48b2676
nikamgeliashvili <mgeliashvilia@gmail.com>
2020-04-02 18:07:56 +0400

    validation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 $(".see-more").click(function () {
     $(".more-info-text").slideToggle();
 });
+
+
+
+
+
+
+// var name = $('#fname').val().length;
+// if (name < 3) {
+//   $("#fname").css('color','red');
+//  }
+
+
+
+ $('.review-section input,textarea').keyup(function() {
+
+  if(this.value.length < 4){
+    $(this).css('color','red')
+  }
+  else{
+    $('.review-section input,textarea').css('color','black')
+  }
+});
+
+
